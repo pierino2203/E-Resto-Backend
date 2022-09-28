e9f95272ff7128428240f0ba7847c0b6084a29a4
pierino esteban juncos <pierino2203>
2022-09-28 10:39:09 -0300

    Rutas get Product

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import usersRouter from "./usersRoutes";
-import ordersRouter from './ordersRoutes'
+import ordersRouter from './ordersRoutes';
+import productRouter from './productRoutes'
 
 const router = Router()
 router.use('/',usersRouter)
 router.use('/',ordersRouter)
+router.use('/',productRouter)
 export default router;
 
 
