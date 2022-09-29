a0adb6aad36b5f3e43c6cdaaebb5b025fe149200
lubermudezz <bermudezluciana@hotmail.com>
2022-09-29 13:53:40 -0300

    modified productController getProducts funciona ahora con query

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,13 +1,42 @@
 import { RequestHandler } from 'express'
-import { isValidObjectId } from 'mongoose'
 import Product from '../models/Products'
 
-export const getProduct: RequestHandler = async (req,res) =>  {
+export const getProduct : RequestHandler = async (req, res) =>  {
   try {
+    let {name} = req.query
     const product = await Product.find()
+    if(name) {
+      let nombre = name.toString()
+      let products = product.filter(e => e.name.toLowerCase().includes(nombre?.toLowerCase()))
+        if(!products.length) {
+          return res.send(product)
+        } else {
+            return res.json(products)
+        }
+    }
     res.status(200).json(product)
   } catch (error) {
     console.log('Error in get Products',error)
   }
 
 }
+
+/*
+
+    try {
+      let {name} = req.query
+      const product = await Product.find()
+      if(name) {
+        let products = product.filter(e => e.name.toLowerCase().includes(name?.toLowerCase()))
+          if(!products.length) {
+            return res.send(product)
+          } else {
+              return res.json(products)
+          }
+      }
+      res.status(200).json(product)
+    } catch (error) {
+      console.log('Error in get Products',error)
+    }
+*/
+
