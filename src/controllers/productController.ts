399d93671569f0c00f82541b1124f42a0dd527cc
lubermudezz <bermudezluciana@hotmail.com>
2022-09-29 14:02:28 -0300

    delete productController comments

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -21,22 +21,3 @@ export const getProduct : RequestHandler = async (req, res) =>  {
 
 }
 
-/*
-
-    try {
-      let {name} = req.query
-      const product = await Product.find()
-      if(name) {
-        let products = product.filter(e => e.name.toLowerCase().includes(name?.toLowerCase()))
-          if(!products.length) {
-            return res.send(product)
-          } else {
-              return res.json(products)
-          }
-      }
-      res.status(200).json(product)
-    } catch (error) {
-      console.log('Error in get Products',error)
-    }
-*/
-
