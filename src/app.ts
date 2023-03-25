39c24dfe504447faa2dcfa4d9d1222d394353735
Robles Lautaro <lautaro0121@gmail.com>
2023-03-25 01:07:40 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,9 @@ const bodyParser = require ('body-parser')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(morgan('dev'));
-app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use('/',router)
